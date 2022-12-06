a55f2054c5280685d910bc9b7d9f663f1b148247
Brandon Mitchell <git@bmitch.net>
2022-12-06 05:18:51 -0500

    2022 Day 6
    Signed-off-by: Brandon Mitchell <git@bmitch.net>

---
+++ 2022/06-b/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+const uniqueLen = 14
+
+func main() {
+	inB, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to read input: %v", err)
+		return
+	}
+	in := string(inB)
+	in = strings.TrimSpace(in)
+	uniqBuf := [uniqueLen]rune{}
+	for i, c := range in {
+		if i < uniqueLen-1 {
+			uniqBuf[i] = c
+			continue
+		}
+		uniqBuf[i%uniqueLen] = c
+		if isUnique(uniqBuf) {
+			fmt.Printf("found unique range of %d ending at %d\n", uniqueLen, i+1)
+			break
+		}
+	}
+}
+
+func isUnique(r [uniqueLen]rune) bool {
+	for start := 0; start < uniqueLen-1; start++ {
+		for offset := start + 1; offset < uniqueLen; offset++ {
+			if r[start] == r[offset] {
+				return false
+			}
+		}
+	}
+	return true
+}
